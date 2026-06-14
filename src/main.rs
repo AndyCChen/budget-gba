926cc445ff71d22e5c6cd05c9fc751579de0312c
Andy Chen <andychungchen@gmail.com>
2026-06-13 21:13:20 -0700

    take in path to gamepak from as command line argument with clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use app::*;
+use clap::Parser;
 use gba::GbaCoreConfig;
 use std::path::PathBuf;
 
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    /// Path to bios file (.bin)
+    #[arg(short, value_name = "FILE")]
+    bios_path: Option<PathBuf>,
+
+    /// Path to gamepak file (.gba)
+    #[arg(short, value_name = "FILE")]
+    gamepak_path: Option<PathBuf>,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let gba_config = GbaCoreConfig {
-        bios_path: PathBuf::from(format!("resource/gba_bios.bin",)),
-        gamepak_path: Some(PathBuf::from("resource/first.gba")),
+        bios_path: PathBuf::from("resource/gba_bios.bin"),
+        gamepak_path: args.gamepak_path,
     };
 
     let config = Config { gba_config };
